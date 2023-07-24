6e6634ab60780a144d6b4ee8da2cd2262c1ab6cc
pimentacaroline <pimenta.caroline@gmail.com>
2023-07-24 16:20:01 +0200

    created pokemonList variable

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+let pokemonList = [
+	{ name: 'Pikachu', height: 5, type: ['water', 'grass'] },
+	{ name: 'Snorlax', height: 7, type: ['fire', 'mountain'] },
+	{ name: 'Charizard', height: 9, type: ['air', 'poison'] }
+];
