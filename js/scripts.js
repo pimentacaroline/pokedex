6f90ac31f8f38784e34824e0d48109cc1b32a61d
pimentacaroline <pimenta.caroline@gmail.com>
2023-07-24 16:31:08 +0200

    Rewrite the code for clarity

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,17 @@
 let pokemonList = [
-	{ name: 'Pikachu', height: 5, type: ['water', 'grass'] },
-	{ name: 'Snorlax', height: 7, type: ['fire', 'mountain'] },
-	{ name: 'Charizard', height: 9, type: ['air', 'poison'] }
+	{
+		name: 'Pikachu', 
+		height: 5, 
+		type: ['water', 'grass']
+	},
+	{
+		name: 'Snorlax', 
+		height: 7, 
+		type: ['fire', 'mountain']
+	},
+	{
+		name: 'Charizard', 
+		height: 9, 
+		type: ['air', 'poison']
+	}
 ];
