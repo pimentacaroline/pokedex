a63fdaf4a22b1036fe84f3a4410fd5648c88fe52
pimentacaroline <pimenta.caroline@gmail.com>
2023-07-25 13:08:30 +0200

    write the pokemon name and height in the DOM

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// list of pokemons
 let pokemonList = [
 	{
 		name: 'Pikachu', 
@@ -15,3 +16,15 @@ let pokemonList = [
 		type: ['air', 'poison']
 	}
 ];
+
+//write the pokemon name and height in the DOM and highlight one
+
+for (i=0; i < pokemonList.length; i++) {
+	if (pokemonList[i].height > 7){
+		document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') - Wow, that\’s big!' + '<br>');
+	} else {
+		document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')' + '<br>');
+	}
+}
+
+
