aa89bb17405aed79d960f6fc7e65c81f92003719
pimentacaroline <pimenta.caroline@gmail.com>
2023-07-26 14:44:44 +0200

    Wrapped pokemonList array in an IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,44 @@
 // list of pokemons
-let pokemonList = [
-	{
-		name: 'Pikachu', 
-		height: 5, 
-		type: ['water', 'grass']
-	},
-	{
-		name: 'Snorlax', 
-		height: 7, 
-		type: ['fire', 'mountain']
-	},
-	{
-		name: 'Charizard', 
-		height: 9, 
-		type: ['air', 'poison']
-	}
-];
+
+let pokemonRepository = (function () {
+		
+		let pokemonList = [
+			{
+				name: 'Pikachu', 
+				height: 5, 
+				type: ['water', 'grass']
+			},
+			{
+				name: 'Snorlax', 
+				height: 7, 
+				type: ['fire', 'mountain']
+			},
+			{
+				name: 'Charizard', 
+				height: 9, 
+				type: ['air', 'poison']
+			}
+		];
+
+		function getAll () {
+			return pokemonList;
+		}
+		function add(item) {
+			pokemonList.push(item);
+		}
+
+		return {
+			getAll: getAll,
+			add: add
+		}
+
+}) ()
+
+
+
 
 // forEach() function to iterarte over the pokemon list
-pokemonList.forEach(function(pokemon) {
+pokemonRepository.getAll().forEach(function(pokemon) {
 	if (pokemon.height > 7){
 		document.write(pokemon.name + ' (height: ' + pokemon.height + ') - Wow, that\’s big!' + '<br>');
 	} else {
